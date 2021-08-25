9a87f8d50ce5ab8ab017721f317161f74143bce5
Seng Jik <853974536@qq.com>
2021-08-25 19:02:17 +0800

    Fuck OCaml!

--- YukimiScript.Parser.Test/Program.fs
+++ YukimiScript.Parser.Test/Program.fs
@@ -1,13 +1,4 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+module Program =
 
-open System
-
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
-
-[<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    [<EntryPoint>]
+    let main _ = 0
---
+++ YukimiScript.Parser.Test/UnitTest1.fs
@@ -0,0 +1,11 @@
+module YukimiScript.Parser.Test
+
+open NUnit.Framework
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let Test1 () =
+    Assert.Pass()
