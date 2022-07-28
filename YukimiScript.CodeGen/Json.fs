748ee4537d0121691db04b5e0809b342963e4a70
Seng Jik <xc1998@foxmail.com>
2022-07-28 17:52:47 +0800

    More debug info for json.

--- YukimiScript.CodeGen/Json.fs
+++ YukimiScript.CodeGen/Json.fs
@@ -2,6 +2,15 @@ module YukimiScript.CodeGen.Json
 
 open YukimiScript.Parser
 
+let private writeConstant (writer: System.Text.Json.Utf8JsonWriter) =
+    function
+    | Elements.Symbol "true" -> writer.WriteBooleanValue(true)
+    | Elements.Symbol "false" -> writer.WriteBooleanValue(false)
+    | Elements.Symbol "null" -> writer.WriteNullValue()
+    | Elements.String x -> writer.WriteStringValue(x)
+    | Elements.Symbol x -> writer.WriteStringValue(x)
+    | Elements.Integer x -> writer.WriteNumberValue(x)
+    | Elements.Real x -> writer.WriteNumberValue(x)
 
 let genJson debug (Intermediate scenes) targetFile = 
     use fileStream = 
@@ -12,17 +21,36 @@ let genJson debug (Intermediate scenes) targetFile =
 
     writer.WriteStartArray()
 
-    let writeDebugInfo (debugInfo: Elements.DebugInfo) =
-        if debug then
-            writer.WriteStartObject("debug")
-            writer.WriteString("src", debugInfo.File)
-            writer.WriteNumber("line", debugInfo.LineNumber)
-            writer.WriteEndObject()
+    let rec writeDebugInfo (debugInfo: Elements.DebugInfo) =
+        writer.WriteString("src", debugInfo.File)
+        writer.WriteNumber("line", debugInfo.LineNumber)
+        match debugInfo.Scope with
+        | Some (Choice1Of2 a) -> 
+            writer.WriteString ("scope_type", "macro")
+            writer.WriteString ("macro", a.Name)
+        | Some (Choice2Of2 a) ->
+            writer.WriteString ("scope_type", "scene")
+            writer.WriteString ("scene", a.Name)
+        | None ->
+            writer.WriteNull ("scope_type")
+        writer.WriteStartObject ("vars")
+        for (name, var) in debugInfo.MacroVars do
+            writer.WritePropertyName name
+            writeConstant writer var
+        writer.WriteEndObject ()
+
+        writer.WriteStartObject ("outter")
+        debugInfo.Outter |> Option.iter writeDebugInfo
+        writer.WriteEndObject ()        
 
     for scene in scenes do 
         writer.WriteStartObject()
         writer.WriteString("scene", scene.Name)
-        writeDebugInfo scene.DebugInformation
+
+        if debug then
+            writer.WriteStartObject("debug")
+            writeDebugInfo scene.DebugInformation
+            writer.WriteEndObject()
         
         begin
             writer.WriteStartArray("block")
@@ -31,16 +59,13 @@ let genJson debug (Intermediate scenes) targetFile =
                 writer.WriteString("call", call.Callee)
                 writer.WriteStartArray("args")
                 for arg in call.Arguments do
-                    match arg with
-                    | Elements.Symbol "true" -> writer.WriteBooleanValue(true)
-                    | Elements.Symbol "false" -> writer.WriteBooleanValue(false)
-                    | Elements.Symbol "null" -> writer.WriteNullValue()
-                    | Elements.String x -> writer.WriteStringValue(x)
-                    | Elements.Symbol x -> writer.WriteStringValue(x)
-                    | Elements.Integer x -> writer.WriteNumberValue(x)
-                    | Elements.Real x -> writer.WriteNumberValue(x)
+                    writeConstant writer arg
+                    
                 writer.WriteEndArray()
-                writeDebugInfo call.DebugInformation
+                if debug then
+                    writer.WriteStartObject("debug")
+                    writeDebugInfo call.DebugInformation
+                    writer.WriteEndObject()
                 writer.WriteEndObject()
             writer.WriteEndArray()
         end
