0e3ece53f6a32ef05728ac533c1db15ead4368d9
Seng Jik <xc1998@foxmail.com>
2022-08-20 14:14:02 +0800

    Compile Pipe

--- YukimiScript.Parser/CompilePipe.fs
+++ YukimiScript.Parser/CompilePipe.fs
@@ -40,3 +40,80 @@ let findLibs libDirs libs =
     
     if fails <> [] then Error <| CanNotFindLib fails else Ok succs
 
+
+let loadSrcs paths =
+    List.map loadDom paths
+    |> ParserMonad.sequenceRL
+    |> Result.map (List.fold Dom.merge Dom.empty)
+
+
+let checkRepeat (dom: Dom) =
+    let findRepeat (items: (string * Elements.DebugInfo) seq) =
+        Seq.groupBy fst items
+        |> Seq.choose
+            (fun (key, matches) ->
+                if Seq.length matches <= 1 then
+                    None
+                else
+                    Some(key, matches |> Seq.map snd))
+                    
+    dom.Externs
+    |> Seq.map (fun (Elements.ExternCommand (cmd, _), _, dbg) -> cmd, dbg)
+    |> findRepeat
+    |> Seq.tryHead
+    |> function
+        | None -> Ok dom
+        | Some x ->
+            Dom.ExternRepeatException x
+            |> Error
+    |> Result.bind (fun dom ->
+        dom.Scenes
+        |> Seq.map (fun (s, _, dbg) -> s.Name, dbg)
+        |> findRepeat
+        |> Seq.tryHead
+        |> function
+            | None -> Ok dom
+            | Some x ->
+                Dom.SceneRepeatException x
+                |> Error)
+    |> Result.bind (fun dom ->
+        dom.Macros
+        |> Seq.map (fun (s, _, _, dbg) -> s.Name, dbg)
+        |> findRepeat
+        |> Seq.tryHead
+        |> function
+            | None -> Ok dom
+            | Some x ->
+                Dom.MacroRepeatException x
+                |> Error)
+
+
+let checkLib = Ok
+
+
+let loadLibs = loadSrcs >> Result.bind checkLib >> Result.bind checkRepeat
+
+
+let getYkmFiles (path: string) =
+    if File.Exists path
+    then [|path|]
+    else
+        Directory.EnumerateFiles(path, "*.ykm", SearchOption.AllDirectories)
+        |> Array.ofSeq
+
+
+let generateIntermediate dom =
+    checkRepeat dom
+    |> Result.bind (Dom.expandTextCommands >> Dom.expandUserMacros)
+    |> Result.bind (fun externAndSysMacro ->
+        Dom.expandSystemMacros externAndSysMacro
+        |> Dom.linkToExternCommands
+        |> Result.map Intermediate.ofDom)
+
+
+let compile lib srcPath =
+    loadDom srcPath
+    |> Result.map (Dom.merge lib)
+    |> Result.bind generateIntermediate
+    
+
